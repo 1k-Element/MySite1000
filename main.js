9bb09cbb31af94e06206f0c6b578d179d46a24be
SquareRoot of a Million <84550028+1k-Element@users.noreply.github.com>
2023-07-09 23:15:51 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -7,65 +7,47 @@ req.onload = function(){
 req.send();
 
 
+        // JavaScript to handle the popup functionality
+        var output1 = document.getElementById("myOutput");
+        var img1 = document.querySelector(" .masky .icons .button1 img");
+        var span1 = document.getElementsByClassName("close1")[0];
 
 
-document.querySelector('.button1').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'flex';
-  });
-  document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'none';
-  });
 
 
-document.querySelector('.button2').addEventListener('click', function() {
-    document.querySelector('#profile').style.display = 'flex';
-  });
-  document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('#profile').style.display = 'none';
-  });
 
-document.querySelector('.button3').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'flex';
-  });
-  document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'none';
-  });
+        var profile2 = document.getElementById("myProfile");
+        var img2 = document.querySelector(" .masky .icons .button2 img");
+        var span2 = document.getElementsByClassName("close2")[0];
 
 
-document.querySelector('.button4').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'flex';
-  });
-  document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'none';
-  });
-
-document.querySelector('.button5').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'flex';
-  });
-  document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'none';
-  });
 
 
-document.querySelector('.button6').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'flex';
-  });
-  document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'none';
-  });
+        img1.onclick = function () {
+            output1.style.display = "block";}
+
+        span1.onclick = function () {
+            output1.style.display = "none";}
 
 
-  document.querySelector('.button7').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'flex';
-  });
-  document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'none';
-  });
 
 
-document.querySelector('.button8').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'flex';
-  });
-  document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('#contact').style.display = 'none';
-  });
+        img2.onclick = function () {
+            profile2.style.display = "block";}
+
+        span2.onclick = function () {
+            profile2.style.display = "none";}
+
+
+
+
+
+        window.onclick = function (event) {
+            if (event.target == output1) {
+                output1.style.display = "none";
+            }
+
+            else if (event.target == profile2) {
+                profile2.style.display = "none";
+            }
+        }
